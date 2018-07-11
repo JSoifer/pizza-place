640bab57330569c0c3178e4d810e7266c87b3976
Julianne Soifer <juliannesoifer@gmail.com>
2018-07-10 20:43:05 -0700

    create a Pizza object and begin a price prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
-$(document).ready(function(){
-  $("form#build-your-own").submit(function(event){
+function Pizza(size, [toppings], price) {
+  this.size = size;
+  this.toppings = [toppings];
+  this.price = price;
+}
+
+Pizza.prototype.price = function() {
+  var mediumPrice = 12;
+  var largePrice = 15;
+
+  for (var i = 0; i)
+
+}
+
+
+
+
+$(document).ready(function() {
+  var newPizza;
+  $("form#build-your-own").submit(function(event) {
     event.preventDefault();
     $("#pizza-order").empty();
-    $("#pizza-order").show();
+
     var size = $("input:radio[name=size]:checked").val();
     $("input:checkbox[name=veggie]:checked").each(function() {
       var veggieToppings = $(this).val();
     $("input:checkbox[name=meat]:checked").each(function() {
       var meatToppings = $(this).val();
-      $('#pizza-order').append(veggieToppings + meatToppings + "<br>");
+      $('#pizza-order').append(veggieToppings + "<br>" + meatToppings + "<br>");
+      $("#pizza-order").show();
      });
     });
   });
