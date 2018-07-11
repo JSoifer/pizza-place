995df53f0a5a287c4f65f6b904494f7cf5a680ed
Julianne Soifer <juliannesoifer@gmail.com>
2018-07-10 22:20:39 -0700

    Added price prototype. does not work.

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,44 @@ function Pizza(size, [toppings], price) {
   this.toppings = [toppings];
   this.price = price;
 }
+var toppings = [];
 
 Pizza.prototype.price = function() {
-  var mediumPrice = 12;
-  var largePrice = 15;
+  var price = 0
 
-  for (var i = 0; i)
+  if (this.size === "large") {
+    price = 15;
+  } else if (this.size === "medium") {
+    price = 12;
+  }
+  if (this.toppings.length === 0) {
+    price *= 1;
+  } else if ((this.size === "large") && (this.toppings.length > 0)) {
+    price = this.toppings.length *= 3;
+  } else if ((this.size === "medium") && (this.toppings.length >0)) {
+    price = this.toppings.length *= 2;
+  }
+  return price;
+};
 
-}
+
 
 
 
 
 $(document).ready(function() {
-  var newPizza;
   $("form#build-your-own").submit(function(event) {
     event.preventDefault();
     $("#pizza-order").empty();
-
     var size = $("input:radio[name=size]:checked").val();
     $("input:checkbox[name=veggie]:checked").each(function() {
       var veggieToppings = $(this).val();
+      toppings.push(veggieToppings);
     $("input:checkbox[name=meat]:checked").each(function() {
       var meatToppings = $(this).val();
-      $('#pizza-order').append(veggieToppings + "<br>" + meatToppings + "<br>");
+      toppings.push(meatToppings);
+    var newPizza =
+      $('#pizza-order').text(newPizza);
       $("#pizza-order").show();
      });
     });
