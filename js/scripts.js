a04b0a18ab3b0624c76baecb23f0a46ffabae89b
Julianne Soifer <juliannesoifer@gmail.com>
2018-07-11 21:07:14 -0700

    work on price prototype. Have a syntax error at very end of user interface script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function Pizza(size, [toppings], price) {
   this.size = size;
-  this.toppings = [toppings];
   this.price = price;
+  this.toppings = [];
 }
 var toppings = [];
+var toppingsAdd = toppings.length;
 
 Pizza.prototype.price = function() {
   var price = 0
@@ -12,14 +13,10 @@ Pizza.prototype.price = function() {
     price = 15;
   } else if (this.size === "medium") {
     price = 12;
+  for (i = 0; toppingsAdd > 0; i *= 2) {
+    price =+ i;
   }
-  if (this.toppings.length === 0) {
-    price *= 1;
-  } else if ((this.size === "large") && (this.toppings.length > 0)) {
-    price = this.toppings.length *= 3;
-  } else if ((this.size === "medium") && (this.toppings.length >0)) {
-    price = this.toppings.length *= 2;
-  }
+
   return price;
 };
 
@@ -32,17 +29,18 @@ $(document).ready(function() {
   $("form#build-your-own").submit(function(event) {
     event.preventDefault();
     $("#pizza-order").empty();
+
     var size = $("input:radio[name=size]:checked").val();
     $("input:checkbox[name=veggie]:checked").each(function() {
       var veggieToppings = $(this).val();
       toppings.push(veggieToppings);
+    });
     $("input:checkbox[name=meat]:checked").each(function() {
       var meatToppings = $(this).val();
       toppings.push(meatToppings);
-    var newPizza =
-      $('#pizza-order').text(newPizza);
-      $("#pizza-order").show();
-     });
     });
+    var newPizza = new Pizza(size, [toppings], price);
+      $("#pizza-order").text(newPizza);
+      $("#pizza-order").show();
   });
 });
